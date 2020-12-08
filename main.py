13a7a19c082f7123c44ed8df1aad7a3a0b0a47e3
Edison Li <edison4354@gmail.com>
2020-12-08 01:41:03 -0800

    Inital setup for pygame screen

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ import pygame, sys
 pygame.init()
 clock = pygame.time.Clock()
 
-# Setting up the main window
+# Setting up the main screen for displaying the game
 screen_width = 1280 
 screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
+
+while True:
+  # Handling input for exiting the page 
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
+  
+  # Updating the window
+  pygame.display.flip()
+  # Limits the loop to run 60 fps
+  clock.tick(60)
