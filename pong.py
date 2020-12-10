ffd308dc37cba57d277bc22822d527fd295de45f
Edison Li <edison4354@gmail.com>
2020-12-10 07:45:47 -0800

    Created Game Manager Class and finishing touches

--- pong.py
+++ pong.py
@@ -85,7 +85,41 @@ class Opponent(Block):
 		if self.rect.top <= 0: self.rect.top = 0
 		if self.rect.bottom >= screen_height: self.rect.bottom = screen_height
 
-# class GameManager:
+class GameManager:
+	def __init__(self,ball_group,paddle_group):
+		self.player_score = 0
+		self.opponent_score = 0
+		self.ball_group = ball_group
+		self.paddle_group = paddle_group
+
+	def run_game(self):
+		# Drawing the game objects
+		self.paddle_group.draw(screen)
+		self.ball_group.draw(screen)
+
+		# Updating the game objects
+		self.paddle_group.update(self.ball_group)
+		self.ball_group.update()
+		self.reset_ball()
+		self.draw_score()
+
+	def reset_ball(self):
+		if self.ball_group.sprite.rect.right >= screen_width:
+			self.opponent_score += 1
+			self.ball_group.sprite.reset_ball()
+		if self.ball_group.sprite.rect.left <= 0:
+			self.player_score += 1
+			self.ball_group.sprite.reset_ball()
+
+	def draw_score(self):
+		player_score = basic_font.render(str(self.player_score),True,accent_color)
+		opponent_score = basic_font.render(str(self.opponent_score),True,accent_color)
+
+		player_score_rect = player_score.get_rect(midleft = (screen_width / 2 + 40,screen_height/2))
+		opponent_score_rect = opponent_score.get_rect(midright = (screen_width / 2 - 40,screen_height/2))
+
+		screen.blit(player_score,player_score_rect)
+		screen.blit(opponent_score,opponent_score_rect)
 
 # Inital setup for pygame
 pygame.init()
@@ -113,6 +147,8 @@ ball = Ball('Ball.png', screen_width/2, screen_height/2, 4, 4, paddle_group)
 ball_sprite = pygame.sprite.GroupSingle()
 ball_sprite.add(ball)
 
+game_manager = GameManager(ball_sprite,paddle_group)
+
 while True:
   # Handling input
   for event in pygame.event.get():
@@ -134,6 +170,9 @@ while True:
   # Background
   screen.fill(bg_color)
   pygame.draw.rect(screen, accent_color, middle_strip)
+
+  # Run the game
+	game_manager.run_game()
 
   # Rendering and updating the window
   pygame.display.flip()
