c2d05b83c1b0350200ace1a470b3e340518c377a
Edison Li <edison4354@gmail.com>
2020-12-10 09:05:01 -0800

    Finished

--- main.py
+++ main.py
@@ -1,94 +1,176 @@
 import pygame, sys, random
 
-def ball_animation():
-  global ball_speed_x, ball_speed_y
+class Block(pygame.sprite.Sprite):
+  def __init__(self, path, x_pos, y_pos):
+    super().__init__()
+    self.image = pygame.image.load(path)
+    self.rect = self.image.get_rect(center = (x_pos, y_pos))
 
-  ball.x += ball_speed_x
-  ball.y += ball_speed_y
+class Player(Block):
+	def __init__(self,path,x_pos,y_pos,speed):
+		super().__init__(path,x_pos,y_pos)
+		self.speed = speed
+		self.movement = 0
 
-  if ball.top <= 0 or ball.bottom >= screen_height:
-    ball_speed_y *= -1
-  if ball.left <= 0 or ball.right >= screen_width:
-    ball_restart()
-  
-  if ball.colliderect(player) or ball.colliderect(opponent):
-    ball_speed_x *= -1
+  #Keeps the player from moving out of the screen
+	def screen_constrain(self):
+		if self.rect.top <= 0:
+			self.rect.top = 0
+		if self.rect.bottom >= screen_height:
+			self.rect.bottom = screen_height
 
-def player_animation():
-  player.y += player_speed
-  if player.top <= 0:
-    player.top = 0 
-  if player.bottom >= screen_height:
-    player.bottom = screen_height
+  # Moves the y cooridates of the player paddle when up and down button are used
+	def update(self,ball_group):
+		self.rect.y += self.movement
+		self.screen_constrain()
 
-def opponent_ai():
-  if opponent.top < ball.y:
-    opponent.top += opponent_speed
-  if opponent.bottom > ball.y:
-    opponent.bottom -= opponent_speed
-  if opponent.top <= 0:
-    opponent.top = 0 
-  if opponent.bottom >= screen_height:
-    opponent.bottom = screen_height
+class Ball(Block):
+	def __init__(self,path,x_pos,y_pos,speed_x,speed_y,paddles):
+		super().__init__(path,x_pos,y_pos)
+		self.speed_x = speed_x * random.choice((-1,1))
+		self.speed_y = speed_y * random.choice((-1,1))
+		self.paddles = paddles
+		self.active = False
 
-def ball_restart():
-  global ball_speed_x, ball_speed_y
-  ball.center = (screen_width/2, screen_height/2)
-  ball_speed_y += random.choice((1,-1))
-  ball_speed_x += random.choice((1,-1))
+  # Moves the x and y position of the ball and detectis
+	def update(self):
+		if self.active:
+			self.rect.x += self.speed_x
+			self.rect.y += self.speed_y
+			self.collisions()
+		else:
+			self.reset_ball()
+		
+	def collisions(self):
+    # Checks if the ball collides with either the top or bottom of the screen
+		if self.rect.top <= 0 or self.rect.bottom >= screen_height:
+			self.speed_y *= -1
 
-# Inital setup 
+    # Checks if the ball collides with the paddle
+		if pygame.sprite.spritecollide(self,self.paddles,False):
+			collision_paddle = pygame.sprite.spritecollide(self,self.paddles,False)[0].rect
+			if abs(self.rect.right - collision_paddle.left) < 10 and self.speed_x > 0:
+				self.speed_x *= -1
+			if abs(self.rect.left - collision_paddle.right) < 10 and self.speed_x < 0:
+				self.speed_x *= -1
+			if abs(self.rect.top - collision_paddle.bottom) < 10 and self.speed_y < 0:
+				self.rect.top = collision_paddle.bottom
+				self.speed_y *= -1
+			if abs(self.rect.bottom - collision_paddle.top) < 10 and self.speed_y > 0:
+				self.rect.bottom = collision_paddle.top
+				self.speed_y *= -1
+
+	def reset_ball(self):
+    # Sets the ball back to the center and moves the ball to a random directions 
+		self.active = True
+		self.speed_x *= random.choice((-1,1)) 
+		self.speed_y *= random.choice((-1,1))
+		self.rect.center = (screen_width/2,screen_height/2)
+
+class Opponent(Block):
+	def __init__(self,path,x_pos,y_pos,speed):
+		super().__init__(path,x_pos,y_pos)
+		self.speed = speed
+
+  # This function is made for the paddle to follow the ball y
+	def update(self,ball_group):
+		if self.rect.top < ball_group.sprite.rect.y:
+			self.rect.y += self.speed
+		if self.rect.bottom > ball_group.sprite.rect.y:
+			self.rect.y -= self.speed
+		self.constrain()
+
+  # Keeps the paddle from going out of the screen
+	def constrain(self):
+		if self.rect.top <= 0: self.rect.top = 0
+		if self.rect.bottom >= screen_height: self.rect.bottom = screen_height
+
+class GameManager:
+	def __init__(self,ball_group,paddle_group):
+		self.player_score = 0
+		self.opponent_score = 0
+		self.ball_group = ball_group
+		self.paddle_group = paddle_group
+
+	def run_game(self):
+		# Drawing the game objects
+		self.paddle_group.draw(screen)
+		self.ball_group.draw(screen)
+
+		# Updating the game objects
+		self.paddle_group.update(self.ball_group)
+		self.ball_group.update()
+		self.reset_ball()
+		self.draw_score()
+
+	def reset_ball(self):
+		if self.ball_group.sprite.rect.right >= screen_width:
+			self.opponent_score += 1
+			self.ball_group.sprite.reset_ball()
+		if self.ball_group.sprite.rect.left <= 0:
+			self.player_score += 1
+			self.ball_group.sprite.reset_ball()
+
+	def draw_score(self):
+		player_score = basic_font.render(str(self.player_score),True,accent_color)
+		opponent_score = basic_font.render(str(self.opponent_score),True,accent_color)
+
+		player_score_rect = player_score.get_rect(midleft = (screen_width / 2 + 40,screen_height/2))
+		opponent_score_rect = opponent_score.get_rect(midright = (screen_width / 2 - 40,screen_height/2))
+
+		screen.blit(player_score,player_score_rect)
+		screen.blit(opponent_score,opponent_score_rect)
+
+# Inital setup for pygame
 pygame.init()
 clock = pygame.time.Clock()
 
-# Setting up the main screen for displaying the game
-screen_width = 1280 
+# Setting up the main screen for displaying window
+screen_width = 1280
 screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
 
-# Game Rectangles for storing and manipulating certain areas
-ball = pygame.Rect(screen_width/2 - 15 , screen_height/2 - 15, 30, 30)
-player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
-opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+# Global Variables
+bg_color = pygame.Color("black")
+accent_color = pygame.Color("white")
+basic_font = pygame.font.Font('freesansbold.ttf', 32)
+middle_strip = pygame.Rect(screen_width/2 - 2, 0, 4, screen_height)
 
-bg_color = pygame.Color('grey12')
-light_grey = (200, 200, 200)
+# Game Objects
+player = Player('Paddle.png', screen_width - 20, screen_height/2, 5)
+opponent = Opponent('Paddle.png', 20, screen_width/2, 5)
+paddle_group = pygame.sprite.Group()
+paddle_group.add(player)
+paddle_group.add(opponent)
 
-ball_speed_x = 7 * random.choice((1,-1))
-ball_speed_y = 7 * random.choice((1,-1))
-player_speed = 0 
-opponent_speed = 7
+ball = Ball('Ball.png', screen_width/2, screen_height/2, 3, 3, paddle_group)
+ball_sprite = pygame.sprite.GroupSingle()
+ball_sprite.add(ball)
+
+game_manager = GameManager(ball_sprite,paddle_group)
 
 while True:
-  # Handling input
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      sys.exit()
-    if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_DOWN:
-        player_speed += 7
-      if event.key == pygame.K_UP:
-        player_speed -= 7 
-    if event.type == pygame.KEYUP:
-      if event.key == pygame.K_DOWN:
-        player_speed -= 7
-      if event.key == pygame.K_UP:
-        player_speed += 7
-
-  ball_animation()
-  player_animation()
-  opponent_ai()
-
-  # Visuals
-  screen.fill(bg_color)
-  pygame.draw.rect(screen, light_grey, player)
-  pygame.draw.rect(screen, light_grey, opponent)
-  pygame.draw.ellipse(screen, light_grey, ball)
-  pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width/2, screen_height))
-
-  # Updating the window
-  pygame.display.flip()
-  # Limits the loop to run 60 fps
-  clock.tick(60)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP:
+				player.movement -= player.speed
+			if event.key == pygame.K_DOWN:
+				player.movement += player.speed
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_UP:
+				player.movement += player.speed
+			if event.key == pygame.K_DOWN:
+				player.movement -= player.speed
+	
+	# Background Stuff
+	screen.fill(bg_color)
+	pygame.draw.rect(screen,accent_color,middle_strip)
+	# Run the game
+	game_manager.run_game()
+	# Rendering
+	pygame.display.flip()
+	clock.tick(120)
